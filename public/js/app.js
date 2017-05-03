c9218623258e723fa2c04d4d13569c7d97c0a911
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-05-03 17:01:30 +0200

    Added form

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,16 @@ socket.on('message', function(message) {
     console.log('Message received:');
     console.log(message.text);
 });
+
+var $form = $('form');
+
+$form.on('submit', function(e){
+    e.preventDefault();
+    var $message = $form.find('input[name=message]');
+
+    socket.emit('message', {
+        text: $message.val()
+    });
+
+    $message.val('');
+});
