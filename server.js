729c164f2bc828ea818a9635185124e4b1c24f3d
Volodymyr Khrystenko <ovod88@bigmir.net>
2017-05-04 14:58:23 +0200

    Added command @currentUsers to chat

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ var clientInfo = {};
 
 app.use(express.static(__dirname + '/public'));
 
+function sendCurrentUsers(socket) {
+    var info = clientInfo[socket.id];
+    var users = [];
+
+    if(info === 'undefined') {
+        return;
+    }
+
+    Object.keys(clientInfo).forEach(function(socketId) {
+        var userInfo = clientInfo[socketId];
+
+        if(info.room === userInfo.room) {
+            users.push(userInfo.name);
+        }
+    });
+
+    socket.emit('message', {
+        name: 'System',
+        text: 'Current users: ' + users.join(', '),
+        timestamp: moment().valueOf()
+    })
+}
+
 io.on('connection', function(socket) {
     console.log('Client connected to socket.io!');
 
@@ -23,7 +46,7 @@ io.on('connection', function(socket) {
                 timestamp: moment().valueOf()
             });
 
-            delete clientInfo[socket.io];
+            delete clientInfo[socket.id];
         }
     });
 
@@ -40,8 +63,12 @@ io.on('connection', function(socket) {
     socket.on('message', function(message) {
         console.log('Message ' + message.text);
 
-        message.timestamp = moment().valueOf();
-        io.to(clientInfo[socket.id].room).emit('message', message);
+        if(message.text === '@currentUsers') {
+            sendCurrentUsers(socket);
+        } else {
+            message.timestamp = moment().valueOf();
+            io.to(clientInfo[socket.id].room).emit('message', message);
+        }
     });
 
     socket.emit('message', {
